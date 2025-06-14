99e533234a0c2e81fe30bee0671812c167430bdf
Aditya <adityasahoo652@gmail.com>
2025-06-14 10:01:14 +0530

    feat: enhance getSimpleAccountAddress to support AASigner and additional parameters

--- src/getSimpleAddress.ts
+++ src/getSimpleAddress.ts
@@ -1,12 +1,25 @@
 import { ethers, providers } from 'ethers'
+import { AASigner, rpcUserOpSender } from './AASigner'
 
 export async function getSimpleAccountAddress(
   privateKey: string,
-  provider: providers.JsonRpcProvider
+  provider: providers.JsonRpcProvider,
+  entryPointAddress: string,
+  accountFactoryAddress: string
 ): Promise<string> {
   const wallet = new ethers.Wallet(privateKey, provider)
+  const ethersSigner = wallet.connect(provider)
 
-  const owner = await wallet.getAddress()
+  const sendUserOp = rpcUserOpSender(provider, entryPointAddress)
+  const index = parseInt(process.env.AA_INDEX ?? '0')
+  const aaSigner = new AASigner(
+    ethersSigner,
+    entryPointAddress,
+    sendUserOp,
+    accountFactoryAddress,
+    index
+  )
 
-  return owner
+  const smartAccountAddress = await aaSigner.getAddress()
+  return smartAccountAddress
 }
